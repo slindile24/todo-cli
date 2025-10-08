394dac34ccba03f3dafa8e94e76f114017870a54
Slindile <slnkabijhb024@student.wethinkcode.co.za>
2025-10-08 10:40:16 +0200

    was able to retrieve all tasks and was able to display tasks in a nice format

--- src/database.py
+++ src/database.py
@@ -2,28 +2,6 @@
 import sqlite3
 
 
-#Here I am creating a table,this function is only used once at startup
-def create_table():
-
-    connection = sqlite3.connect('tasks.db')
-    cursor = connection.cursor()
-
-    cursor.execute("""CREATE TABLE IF NOT EXISTS tasks (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        title TEXT NOT NULL,
-        description TEXT,
-        completed INTEGER NOT NULL DEFAULT 0,
-        created_at TEXT DEFAULT (datetime('now')),
-        due_date TEXT,
-        priority TEXT
-    );
-    """)
-    connection.commit()
-    connection.close()
-    
-
-create_table()
-
 #This one is used later by other functions to open the same database and work with it.
 def get_connection():
     conn =sqlite3.connect('data/tasks.db')
@@ -40,9 +18,41 @@ def add_task(title,description=None,due_date=None,priority=None):
             VALUES (?, ?, ?, ?)""",
             (title, description, due_date, priority)
     )
-    
     conn.commit()
     conn.close()
-add_task("Wash the dishes","Use hot water","2023-10-10","High")
+add_task("Stand-up meeting","Meeting with team","2023-10-11","High")
+
+def get_all_tasks():
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("""SELECT * FROM tasks""") # This doesn't retrieve data but it just prepares the sql query
+    tasks = cursor.fetchall()   # the fetchall() is used to retrieve data from the database,returns all rows
+    conn.close()    # the fetchall() returns a list of tuples , where each tuple represents one row
+    return tasks
+
+
+def display_tasks():
+    tasks = get_all_tasks()
+    if not tasks:
+        print("No tasks found.")
+        return
+    
+    for task in tasks:
+        task_id, title, description, completed, created_at, due_date, priority= task
+        print(f"""
+
+            ID: {task_id}
+            Title: {title}
+            Description: {description or "None"}
+            Due Date : {due_date or "None"}
+            Priority: {priority or "None"}
+            Completed: {"Yes" if completed  else "No"}
+            Created At: {created_at}
+
+""")
+ 
+display_tasks()
+
+
    
 
