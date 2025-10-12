164383c706398dd6849f12b23cdb9c600667fa0d
Slindile <slnkabijhb024@student.wethinkcode.co.za>
2025-10-12 18:12:01 +0200

    fixed a few errors and added a few features

--- src/main.py
+++ src/main.py
@@ -26,30 +26,50 @@ def display_menu():
         print(menu)
         input_res = input("Choose an option between 1-5: ")
         if input_res == "1":
-            title = input("Enter the task title: ")
-            description = input("Enter the task description: ")
-            due_date = input("Enter the task due_date(YYYY-MM-DD): ")
-            priority = input("Enter the task priority(Low,Medium,High): ")
+            title = input("Enter the task title: ").strip()
+            if not title:
+                print("Task title cannot be empty!")
+                continue
+            title = input("Enter the task title: ").strip()
+
+            description = input("Enter the task description: ").strip()
+            due_date = input("Enter the task due_date(YYYY-MM-DD): ").strip()
+            priority = input("Enter the task priority(Low,Medium,High): ").strip()
+
+            if not due_date:
+                due_date = None
+            if not priority:
+                priority = "Low"
+
             add_task(title,description,due_date,priority)
             print(f"Task '{title}' added successfully! ")
 
         elif input_res == "2":
             print(display_tasks())
-            
+
         elif input_res == "3":
-            update_id = int(input("Enter the task ID you want to update: "))
-            update_task(update_id)
-            print(f"Task {update_id} is updated successfully and marked completed!")
+            try:
+                update_id = int(input("Enter the task ID you want to update: "))
+                update_task(update_id)
+                print(f"Task {update_id} is updated successfully and marked completed!")
+            except ValueError:
+                print("Enter a valid numeric value! ")
+
         elif input_res == "4":
             print(display_tasks())
-            delete_id = int(input("Enter the task ID you want to delete: "))
-            delete_task(delete_id)
-            print(f"Task {delete_id} deleted successfully!")
+            try:
+                delete_id = int(input("Enter the task ID you want to delete: "))
+                delete_task(delete_id)
+                print(f"Task {delete_id} deleted successfully!")
+            except ValueError:
+                print("Enter a valid numeric value! ")
+
         elif input_res == "5":
             print("Exiting!")
             break
         else:
             print("Invalid choice. Pick the correct choice from the menu")
+
 display_menu()
         
     
